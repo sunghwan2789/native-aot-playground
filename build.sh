4c286df1d8b1d61179a7be60e082c45af0873bfe
Sunghwan Bang <sunghwan2789@gmail.com>
2023-02-02 10:00:57 +0000

    Revert "chore: compile Console with Native AOT"
    This reverts commit 455dd052938cb3fb0cd2da34a206e040babd9361.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # dotnet publish -r linux-arm64 -p:NativeLib=Static -c Release -o ./publish
 dotnet publish src/Native -p:PublishProfile=NativeProfile --self-contained -c Release
 dotnet pack src/Library -c Release -o ./packages
-dotnet publish src/Console --self-contained -c Release -o ./publish
+dotnet build src/Console
